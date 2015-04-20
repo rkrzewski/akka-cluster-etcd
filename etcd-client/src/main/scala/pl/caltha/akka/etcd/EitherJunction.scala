d2a6c98bb40fdcd12b81bebc6df590d18b5adff2
Rafał Krzewski <rafal.krzewski@caltha.pl>
2015-04-20 23:07:20 +0200

    Generalized RedirectDecider into a EitherJunction

---
+++ etcd-client/src/main/scala/pl/caltha/akka/etcd/EitherJunction.scala
@@ -0,0 +1,67 @@
+package pl.caltha.akka.etcd
+
+import scala.collection.immutable.Seq
+
+import akka.stream.Inlet
+import akka.stream.Outlet
+import akka.stream.Shape
+import akka.stream.Graph
+import akka.stream.scaladsl.OperationAttributes
+import akka.stream.scaladsl.FlexiRoute
+import akka.stream.scaladsl.FlexiRoute.RouteLogic
+import akka.stream.scaladsl.FlexiRoute.DemandFromAll
+import akka.stream.javadsl.FlexiRoute.State
+
+object EitherJunctionShape {
+  sealed trait Init[I, L, R]
+  case class Name[I, L, R](name: String) extends Init[I, L, R]
+  case class Ports[I, L, R](in: Inlet[I], left: Outlet[L], right: Outlet[R]) extends Init[I, L, R]
+}
+
+import EitherJunctionShape._
+
+class EitherJunctionShape[I, L, R](init: EitherJunctionShape.Init[I, L, R] = Name[I, L, R]("EitherJunction")) extends Shape {
+  private val (_in, _left, _right) = init match {
+    case Name(name) => (new Inlet[I](s"$name.in"), new Outlet[L](s"$name.left"), new Outlet[R](s"$name.right"))
+    case Ports(in, left, right) => (in, left, right)
+  }
+
+  def in: Inlet[I] = _in
+  def left: Outlet[L] = _left
+  def right: Outlet[R] = _right
+
+  def inlets: Seq[Inlet[_]] = Seq(_in)
+
+  def outlets: Seq[Outlet[_]] = Seq(_left, _right)
+
+  private def construct(in: Inlet[I], left: Outlet[L], rigth: Outlet[R]) =
+    new EitherJunctionShape(Ports(in, left, right))
+
+  def deepCopy(): Shape = {
+    construct(new Inlet[I](_in.toString), new Outlet[L](_left.toString), new Outlet[R](_right.toString))
+  }
+
+  def copyFromPorts(inlets: Seq[Inlet[_]], outlets: Seq[Outlet[_]]): Shape = {
+    require(inlets.size == 1, s"proposed inlets [${inlets.mkString(", ")}] do not fit EitherJunctionShape")
+    require(outlets.size == 2, s"proposed outlets [${outlets.mkString(", ")}] do not fit EitherJunctionShape")
+    construct(inlets(0).asInstanceOf[Inlet[I]], outlets(0).asInstanceOf[Outlet[L]], outlets(1).asInstanceOf[Outlet[R]])
+  }
+}
+
+class EitherJunction[I, L, R](f: I => Either[L, R]) extends FlexiRoute[I, EitherJunctionShape[I, L, R]](new EitherJunctionShape, OperationAttributes.name("EitherJunction")) {
+  override def createRouteLogic(p: PortT) = new RouteLogic[I] {
+    override def initialState = State[Any](DemandFromAll(p.left, p.right)) {
+      (ctx, _, element) =>
+        f(element) match {
+          case Left(l) => ctx.emit(p.left)(l)
+          case Right(r) => ctx.emit(p.right)(r)
+        }
+        SameState
+    }
+    override def initialCompletionHandling = eagerClose
+  }
+}
+
+object EitherJunction {
+  def apply[I, L, R](f: I => Either[L, R]) = new EitherJunction(f)
+}
--- etcd-client/src/main/scala/pl/caltha/akka/etcd/HttpRedirects.scala
+++ etcd-client/src/main/scala/pl/caltha/akka/etcd/HttpRedirects.scala
@@ -1,59 +1,21 @@
 package pl.caltha.akka.etcd
 
-import scala.concurrent.Future
+import scala.collection.immutable.Set
 
-import akka.http.Http
 import akka.http.model.HttpRequest
 import akka.http.model.HttpResponse
 import akka.http.model.headers.Location
-import akka.stream.FanOutShape
-import akka.stream.FanOutShape.Init
-import akka.stream.FanOutShape.Name
 import akka.stream.scaladsl.Broadcast
-import akka.stream.scaladsl.FlexiRoute
-import akka.stream.scaladsl.FlexiRoute.DemandFromAll
-import akka.stream.scaladsl.FlexiRoute.RouteLogic
 import akka.stream.scaladsl.Flow
 import akka.stream.scaladsl.FlowGraph
 import akka.stream.scaladsl.Merge
-import akka.stream.scaladsl.OperationAttributes
 import akka.stream.scaladsl.Zip
 
 object HttpRedirects {
+
   def apply(client: Flow[HttpRequest, HttpResponse, _]): Flow[HttpRequest, HttpResponse, Unit] = {
 
-    class RedirectDeciderShape(_init: Init[(HttpRequest, HttpResponse)] = Name[(HttpRequest, HttpResponse)]("RedirectDecider"))
-        extends FanOutShape[(HttpRequest, HttpResponse)](_init) {
-      val redirect = newOutlet[HttpRequest]("redirect")
-      val response = newOutlet[HttpResponse]("response")
-      protected override def construct(i: Init[(HttpRequest, HttpResponse)]) = new RedirectDeciderShape(i)
-    }
-
-    class RedirectDecider extends FlexiRoute[(HttpRequest, HttpResponse), RedirectDeciderShape](
-      new RedirectDeciderShape, OperationAttributes.name("RedirectDecider")) {
-      import FlexiRoute._
-
-      val redirectStatuses = Set(301, 302, 307)
-
-      def redirect(req: HttpRequest): HttpRequest =
-        req.header[Location].map(location => req.withUri(location.uri)).get
-
-      override def createRouteLogic(p: PortT) = new RouteLogic[(HttpRequest, HttpResponse)] {
-        override def initialState = State[Any](DemandFromAll(p.redirect, p.response)) {
-          (ctx, _, elements) =>
-            val (request, response) = elements
-            if (redirectStatuses.contains(response.status.intValue()) &&
-              response.header[Location].isDefined) {
-              ctx.emit(p.redirect)(redirect(request))
-            } else {
-              ctx.emit(p.response)(response)
-            }
-            SameState
-        }
-
-        override def initialCompletionHandling = eagerClose
-      }
-    }
+    val redirectStatuses = Set(301, 302, 307)
 
     Flow[HttpRequest, HttpResponse]() {
       implicit b =>
@@ -62,15 +24,21 @@ object HttpRedirects {
         val bc = b.add(Broadcast[HttpRequest](2))
         val zip = b.add(Zip[HttpRequest, HttpResponse]())
         val originalClient = b.add(client)
-        val decider = b.add(new RedirectDecider)
+        val decider = b.add(EitherJunction[(HttpRequest, HttpResponse), HttpRequest, HttpResponse] {
+          case (request, response) =>
+            if (redirectStatuses.contains(response.status.intValue()) && response.header[Location].isDefined)
+              Left(response.header[Location].map(location => request.withUri(location.uri)).get)
+            else
+              Right(response)
+        })
         val redirectClient = b.add(client)
         val merge = b.add(Merge[HttpResponse](2))
 
         bc ~> zip.in0
         bc ~> originalClient ~> zip.in1
         zip.out ~> decider.in
-        decider.response ~> merge
-        decider.redirect ~> redirectClient ~> merge
+        decider.left ~> redirectClient ~> merge
+        decider.right ~> merge
 
         (bc.in, merge.out)
     }
