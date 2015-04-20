145b5576f50cc319d8b27ac2f3d3722de8ce61f8
Rafał Krzewski <rafal.krzewski@caltha.pl>
2015-04-21 00:35:49 +0200

    test cleanup

--- etcd-client/src/test/scala/pl/caltha/akka/etcd/HttpRedirectsSpec.scala
+++ etcd-client/src/test/scala/pl/caltha/akka/etcd/HttpRedirectsSpec.scala
@@ -1,12 +1,14 @@
 package pl.caltha.akka.etcd
 
+import scala.concurrent.Future
+
 import org.scalatest.Finders
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 import org.scalatest.concurrent.ScalaFutures
+
 import akka.actor.ActorSystem
-import akka.http.marshalling.PredefinedToEntityMarshallers.StringMarshaller
-import akka.http.marshalling.ToResponseMarshallable.apply
+import akka.http.marshalling._
 import akka.http.model.HttpMethods.GET
 import akka.http.model.HttpRequest
 import akka.http.model.HttpResponse
@@ -19,7 +21,6 @@ import akka.stream.ActorFlowMaterializer
 import akka.stream.scaladsl.Flow
 import akka.stream.scaladsl.Sink
 import akka.stream.scaladsl.Source
-import scala.concurrent.Future
 
 class HttpRedirectsSpec extends FlatSpec with Matchers with ScalaFutures {
 
@@ -29,15 +30,17 @@ class HttpRedirectsSpec extends FlatSpec with Matchers with ScalaFutures {
 
   implicit val flowMaterializer = ActorFlowMaterializer()
 
+  implicit val routingSettings = RoutingSettings.default(system)
+
   def mockServerFlow(numRedicects: Int): Flow[HttpRequest, HttpResponse, Unit] = {
     val route = get {
       pathPrefix(IntNumber) { num =>
         if (num < numRedicects)
           overrideStatusCode(StatusCodes.TemporaryRedirect) {
-            respondWithHeader(Location(s"/${num+1}")) {
-            	complete {
-            		"redirect"
-            	}
+            respondWithHeader(Location(s"/${num + 1}")) {
+              complete {
+                "redirect"
+              }
             }
           }
         else {
@@ -47,36 +50,33 @@ class HttpRedirectsSpec extends FlatSpec with Matchers with ScalaFutures {
         }
       }
     }
-    implicit val routingSettings = RoutingSettings.default(system)
-
     Route.handlerFlow(route)
   }
 
-  def request(n: Int, serverFlow: Flow[HttpRequest, HttpResponse, Unit]): Future[HttpResponse] =
-    Source.single(HttpRequest(GET).withUri(s"/$n")).via(serverFlow).runWith(Sink.head)
+  def request(serverFlow: Flow[HttpRequest, HttpResponse, Unit]): Future[HttpResponse] =
+    Source.single(HttpRequest(GET).withUri("/0")).via(serverFlow).runWith(Sink.head)
 
   "redirect-enabled client" should "handle direct response" in {
-    val flow = HttpRedirects.apply(mockServerFlow(0))
-    whenReady(request(0, flow)) {
+    val flow = HttpRedirects.apply(mockServerFlow(numRedicects = 0))
+    whenReady(request(flow)) {
       resp =>
         resp.status shouldBe StatusCodes.OK
     }
   }
-  
- it should "handle single redirect" in {
-    val flow = HttpRedirects.apply(mockServerFlow(1))
-    whenReady(request(0, flow)) {
+
+  it should "handle single redirect" in {
+    val flow = HttpRedirects.apply(mockServerFlow(numRedicects = 1))
+    whenReady(request(flow)) {
       resp =>
         resp.status shouldBe StatusCodes.OK
     }
   }
- 
- it should "report `Too many redirects` on double redirect" in {
-   val flow = HttpRedirects.apply(mockServerFlow(2))
-    whenReady(request(0, flow)) {
+
+  it should "report `Too many redirects` on double redirect" in {
+    val flow = HttpRedirects.apply(mockServerFlow(numRedicects = 2))
+    whenReady(request(flow)) {
       resp =>
         resp.status.intValue() shouldBe 310
     }
- }
-
+  }
 }
