a4d8d8b45d0ca3ed69c902d4934934fecd877a96
Rafał Krzewski <rafal.krzewski@caltha.pl>
2015-04-23 18:54:01 +0200

    simplified HttpRedirects flow

--- etcd-client/src/main/scala/pl/caltha/akka/etcd/HttpRedirects.scala
+++ etcd-client/src/main/scala/pl/caltha/akka/etcd/HttpRedirects.scala
@@ -21,31 +21,25 @@ object HttpRedirects {
    * Augments client flow with the capability to follow HTTP redirects up to a specified depth.
    *
    * {{{
-   *                                   requestNum < maxRedirects +---------------+ 310
-   *                          +----------------------------------|nextReqJunction|--------+
-   *                          |                                  +---------------+        |
-   *                          |                                          ^                v
-   *                          |                                   +----------- +    +-----------+
-   *                          |            +--------------------->| nextReqZip |    | respMerge |--->>>
-   *                          |            |                      +------------+    +-----------+
-   *                          |            |                             ^ 30x            ^
-   *     +--------------+     |            |                    +----------------+ 200    |
-   *     |origReqNum = 0|     |            |                    |redirectJunction|--------+
-   *     +--------------+     |            |                    +----------------+
-   *            v             v            |                            ^
-   *       +----------+   +--------+   +--------+                  +----------+
-   * >>>---|origReqZip|-->|reqMerge|-->|reqUnzip|        +-------->|reqRespZip|
-   *       +----------+   +--------+   +--------+        |         +----------+
-   *                                       |             |              ^
-   *                                       |       +------------+    +------+
-   *                                       +------>|reqBroadcast|--->| http |
-   *                                               +------------+    +------+
+   *                                                                 +----------------+
+   *                          +--------------------------------------|redirectJunction|--->>>
+   *                          |                                      +----------------+
+   *                          |                                               ^
+   *                          |                                               |
+   *       +----------+       |                                         +----------+
+   *       |origReqNum|       |                +----------------------->|reqRespZip|
+   *       +----------+       |                |                        +----------+
+   *            |             |                |                              ^
+   *            v             v                |                              |
+   *       +----------+   +--------+   +---------------+   +----------+   +--------+
+   * >>>---|origReqZip|-->|reqMerge|-->|reqNumBroadcast|-->|extractReq|-->|  http  |
+   *       +----------+   +--------+   +---------------+   +----------+   +--------+
    * }}}
    * @param httpFlow the request to response flow that will be used to contact the server.
-   * @param maxRedirects maximum number of redirects that will be accepted. When server returns a 
-   *        redirect code for the final request, processing will be terminated with `310 Too many 
+   * @param maxRedirects maximum number of redirects that will be accepted. When server returns a
+   *        redirect code for the final request, processing will be terminated with `310 Too many
    *        redirects` response.
-   * @param redirectStatuses HTTP status codes that will be recognized as redirects. The response must contain 
+   * @param redirectStatuses HTTP status codes that will be recognized as redirects. The response must contain
    *        `Location` header, otherwise the response will be passed as-is to the client.
    * @returns augmented request to response flow.
    */
@@ -56,44 +50,32 @@ object HttpRedirects {
       val origReqNum = b.add(Source.repeat(0))
       val origReqZip = b.add(Zip[HttpRequest, Int]())
       val reqMerge = b.add(Merge[(HttpRequest, Int)](2))
-      val reqUnzip = b.add(Unzip[HttpRequest, Int]())
-      val reqBroadcast = b.add(Broadcast[HttpRequest](2))
+      val reqNumBroadcast = b.add(Broadcast[(HttpRequest, Int)](2))
+      val extractReq = b.add(Flow[(HttpRequest, Int)].map { case (req, _) => req })
       val http = b.add(httpFlow)
-      val reqRespZip = b.add(Zip[HttpRequest, HttpResponse]())
-      val redirectJunction = b.add(EitherJunction[(HttpRequest, HttpResponse), HttpRequest, HttpResponse] {
-        case (request, response) =>
+      val reqRespZip = b.add(Zip[(HttpRequest, Int), HttpResponse]())
+      val redirectJunction = b.add(EitherJunction[((HttpRequest, Int), HttpResponse), (HttpRequest, Int), HttpResponse] {
+        case ((request, requestNum), response) =>
           if (redirectStatuses.contains(response.status.intValue()) && response.header[Location].isDefined)
-            Left(response.header[Location].map(location => request.withUri(location.uri)).get)
+            if (requestNum < maxRedirects)
+              Left((response.header[Location].map(location => request.withUri(location.uri)).get, requestNum + 1))
+            else
+              Right(HttpResponse(StatusCodes.custom(310, "Too many redirects")))
           else
             Right(response)
       })
-      val nextReqZip = b.add(Zip[HttpRequest, Int]())
-      val nextReqJunction = b.add(EitherJunction[(HttpRequest, Int), HttpResponse, (HttpRequest, Int)] {
-        case (request, requestNum) =>
-          if (requestNum < maxRedirects)
-            Right((request, requestNum + 1))
-          else
-            Left(HttpResponse(StatusCodes.custom(310, "Too many redirects")))
-      })
-      val respMerge = b.add(Merge[HttpResponse](2))
 
       // in ~> origReqZip.in0
       origReqNum ~> origReqZip.in1
       origReqZip.out ~> reqMerge.in(0)
-      reqMerge.out ~> reqUnzip.in
-      reqUnzip.out0 ~> reqBroadcast.in
-      reqBroadcast.out(0) ~> http.inlet
-      reqBroadcast.out(1) ~> reqRespZip.in0
-      http.outlet ~> reqRespZip.in1
+      reqMerge ~> reqNumBroadcast
+      reqNumBroadcast.out(1) ~> reqRespZip.in0
+      reqNumBroadcast.out(0) ~> extractReq.inlet
+      extractReq ~> http ~> reqRespZip.in1
       reqRespZip.out ~> redirectJunction.in
-      redirectJunction.left ~> nextReqZip.in0
-      reqUnzip.out1 ~> nextReqZip.in1
-      redirectJunction.right ~> respMerge.in(0)
-      nextReqZip.out ~> nextReqJunction.in
-      nextReqJunction.left ~> respMerge.in(1)
-      nextReqJunction.right ~> reqMerge.in(1)
-      // reqMerge.out ~> out
+      redirectJunction.left ~> reqMerge.in(1)
+      // redirectJunction.right ~> out
 
-      (origReqZip.in0, respMerge.out)
+      (origReqZip.in0, redirectJunction.right)
     }
 }
