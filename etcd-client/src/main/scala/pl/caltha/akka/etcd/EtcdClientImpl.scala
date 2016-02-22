bcc5f003f22d5c66371e97a90d5675605ae52cc1
Rafał Krzewski <rafal.krzewski@caltha.pl>
2016-02-23 00:29:04 +0100

    Formatting changes: scalariform control directives

--- etcd-client/src/main/scala/pl/caltha/akka/etcd/EtcdClientImpl.scala
+++ etcd-client/src/main/scala/pl/caltha/akka/etcd/EtcdClientImpl.scala
@@ -88,11 +88,11 @@ private[etcd] class EtcdClientImpl(host: String, port: Int = 4001,
         }))
         val respUnzip = b.add(Unzip[WatchRequest, EtcdResponse]())
 
-        // @formatter:off
+        // @formatter:off format: OFF
         initReq                   ~> reqMerge.in(0)
         respUnzip.out0            ~> reqMerge.in(1)
                                      reqMerge       ~> runWait ~> respUnzip.in
-        // @formatter:on
+        // @formatter:on format: ON
 
         SourceShape(respUnzip.out1)
       }).named("watch").viaMat(FlowBreaker[EtcdResponse])(Keep.right)
